8c9ced8ca3bf5af95994186c347bae47e34c4820
stanstancee <stanstancee@gmail.com>
2019-07-07 13:10:44 +0100

    http fetch demo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import './index.css';
-import numeral from 'numeral';
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`your money is ${courseValue} dollars !`);//eslint-disable-line no-console
+import {getUsers} from './api/userApi';
 
+//  API call.
+getUsers().then(result => {
+  let usersBody = "";
 
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
 
-
+  global.document.getElementById('users').innerHTML = usersBody;
+}
+)
--- src/index.test.js
+++ src/index.test.js
@@ -6,14 +6,14 @@ describe('Our first test', () => {
   it('should pass', () => {
     expect(true).to.equal(true);
   });
-}); 
+});
 
 describe('index.html', () => {
-  it('should have h1 that says Hello', (done) => {
+  it('should have h1 that says Users', (done) => {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Hello World !");
+      expect(h1.innerHTML).to.equal("Users");
       done();
       window.close();
     });
